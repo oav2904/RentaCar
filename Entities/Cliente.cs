7060be59388460935fb96777cca76e47b8a38144
OSCAR ANTONIO ARIAS VARGAS <oariasv@est.utn.ac.cr>
2019-07-17 10:40:41 -0600

    Agregar archivos de proyecto.

---
+++ Entities/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+     public class Cliente
+    {
+        public int ID { set; get;}
+        public string Nombre { set; get; }
+        public string PrimerApellido { get; set; }
+        public string SegundoApellido { get; set; }
+        public int Cedula { get; set; }
+    }
+}
---
+++ Entities/Factura.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Factura
+    {
+        public int ID { get; set; }
+        public int Cliente { get; set; }
+        public int Vehiculo { get; set; }
+        public int Costo { get; set; }
+        public DateTime FechaInicio { get;set; }
+        public DateTime FechaFinal { get; set; }
+
+    }
+}
